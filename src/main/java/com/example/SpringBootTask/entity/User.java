bb1f2acd0f5b4348e579b9596c1bd0756c1c076b
NikaPoliukova <nikanarchuk@gmail.com>
2025-09-03 12:53:58 +0400

    task 5 added Flyway

--- src/main/java/com/example/SpringBootTask/entity/User.java
+++ src/main/java/com/example/SpringBootTask/entity/User.java
@@ -1,13 +1,18 @@
 package com.example.SpringBootTask.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
+
 public class User {
 
     @Id
